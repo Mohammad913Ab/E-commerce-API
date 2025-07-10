3ba776ddf440b9e6d9954b1211cd648656ede536
Mohammad913Ab <mohammad.abolhasani.work@gmail.com>
2025-07-10 08:12:07 +0330

    Write Custom permission(IsAdminOrReadOnly) and add to ProductViewSet

--- products/views.py
+++ products/views.py
@@ -1,9 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
-from .serializers import ProductSerializer
-from .models import Product
 
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+
+from .models import Product
+from .serializers import ProductSerializer
+from core.permissions import IsAdminOrReadOnly
 
 class ProductViewSet(ModelViewSet):
     """
@@ -19,11 +21,18 @@ class ProductViewSet(ModelViewSet):
     - Search by title and description using `?search=...`
     - Ordering by price and created_at using `?ordering=-price`
 
+    Permission behavior:
+    - Read operations (GET) are allowed for all users, including anonymous.
+    - Write operations (POST, PUT, PATCH, DELETE) are restricted to authenticated users with proper permissions (e.g., admin).
+
     Uses ProductSerializer for serialization.
     """
+    permission_classes = [IsAdminOrReadOnly]
+    
     serializer_class = ProductSerializer
     queryset = Product.objects.all()
     
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     search_fields = ['title', 'description']
     ordering_fields = ['price', 'created_at']
+    
