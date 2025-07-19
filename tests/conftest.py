b2f269ac3b76caeadd1f613be542a65e991424b4
Mohammad913Ab <mohammad.abolhasani.work@gmail.com>
2025-07-19 12:35:56 +0330

    Install pytest and change the test writing structure to it & add tests for products

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+from rest_framework.test import APIClient
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+import pytest
+
+from products.models import Product
+
+User = get_user_model()
+
+@pytest.fixture
+def user(db):
+    return User.objects.create(mobile='09986543342', password='Zxcvbnm@123')
+    
+@pytest.fixture
+def product(db):
+    return Product.objects.create(
+        title="ProductTest (1)",
+        price=100
+    )
+
+@pytest.fixture
+def api_client():
+    return APIClient()
---
+++ tests/test_products.py
@@ -0,0 +1,58 @@
+from rest_framework import status
+from django.urls import reverse
+import pytest
+from rest_framework.test import APIClient
+
+from products.models import ProductComment
+
+@pytest.mark.django_db
+class TestProductApi:
+    def test_product_detail(self, api_client, product):
+        assert product.title == "ProductTest (1)"
+        url = reverse('product-detail', kwargs={'pk': product.id})
+        res = api_client.get(url)
+        assert res.status_code == status.HTTP_200_OK
+        assert res.data.get('view_count') > 0
+        
+    def test_product_list(self, api_client):
+        url = reverse('product-list')
+        res = api_client.get(url)
+        data = res.data
+        assert res.status_code == status.HTTP_200_OK
+        assert isinstance(data, dict)
+        assert 'results' in data
+        assert isinstance(data['results'], list)
+        
+    def test_unauthenticated_user_cannot_like(self, api_client, product):
+        url = reverse('product-like', kwargs={'pk': product.id})
+        res = api_client.post(url)
+        assert res.status_code == status.HTTP_401_UNAUTHORIZED or status.HTTP_403_FORBIDDEN
+        
+    def test_user_can_like_product_once(self, api_client, user, product):
+        url = reverse('product-like', kwargs={'pk': product.id})
+        
+        api_client.force_authenticate(user=user)
+
+        res = api_client.post(url)
+        assert res.status_code == status.HTTP_201_CREATED
+        assert res.data['likes'] == 1
+        
+        res = api_client.post(url)
+        assert res.status_code == status.HTTP_400_BAD_REQUEST
+        assert res.data['detail'] == 'You have already like this product.'
+        
+    def test_unauthenticated_user_cannot_comment(self, api_client, product):
+        url = reverse('product-comment', kwargs={'pk': product.id})
+        res = api_client.post(url)
+        assert res.status_code == status.HTTP_401_UNAUTHORIZED or status.HTTP_403_FORBIDDEN
+
+    def test_user_can_like_product(self, api_client:APIClient, user, product):
+        url = reverse('product-comment', kwargs={'pk': product.id})
+        
+        api_client.force_authenticate(user=user)
+        
+        res = api_client.post(url, data={'text':'Comment Test (1)'})
+        assert res.status_code == status.HTTP_201_CREATED
+        assert 'text' in res.data
+        comment = ProductComment.objects.filter(product=product, user=user, text='Comment Test (1)').first()
+        assert comment is not None
