588876bd83dbcaab9c1212f9a17c6a592660fd3b
Mohammad913Ab <mohammad.abolhasani.work@gmail.com>
2025-07-26 11:38:40 +0330

    Add ViewSet for carts

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Cart, CartItem
+from .serializers import CartSerializer, CartItemSerializer
+
+class IsOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
+
+class CartViewSet(viewsets.ModelViewSet):
+    serializer_class = CartSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=['post'])
+    def add_item(self, request, pk=None):
+        cart = self.get_object()
+        serializer = CartItemSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        item, created = CartItem.objects.get_or_create(
+            cart=cart,
+            product=serializer.validated_data['product'],
+            defaults={'quantity': serializer.validated_data['quantity']}
+        )
+        if not created:
+            item.quantity += serializer.validated_data['quantity']
+            item.save()
+        return Response(CartSerializer(cart).data, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['post'])
+    def remove_item(self, request, pk=None):
+        cart = self.get_object()
+        product_id = request.data.get('product_id')
+        CartItem.objects.filter(cart=cart, product_id=product_id).delete()
+        return Response(CartSerializer(cart).data, status=status.HTTP_200_OK)
