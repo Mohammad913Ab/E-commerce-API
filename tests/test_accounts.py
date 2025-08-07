6f5bee7d0b4887b064d4ddac8dba6782fb392c65
Mohammad913Ab <mohammad.abolhasani.work@gmail.com>
2025-08-07 23:06:00 +0330

    Add test to cover 100% of CustomBackend & CustomUserManager in accounts

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -2,9 +2,64 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 from accounts.models import User
+from accounts.custom_backend import CustomBackend
 
 @pytest.mark.django_db
 class TestAccountApi:
+    def test_create_user(self):
+        # User with out mobile
+        with pytest.raises(ValueError) as exc_info:
+            User.objects.create_user(
+                mobile='',
+                password='TestPass123'
+                )
+        assert 'The Mobile must be set' in str(exc_info)
+        
+        # User creation
+        User.objects.create_user(
+            mobile='09123456789',
+            password='TestPass123'
+            )
+        
+        # Superuser create test
+        with pytest.raises(ValueError) as exc_info:
+            User.objects.create_superuser(
+                password='TestPass123',
+                mobile='09123456789',
+                is_staff=False
+                )
+        assert 'Superuser must have is_staff=True.' in str(exc_info)
+        
+        with pytest.raises(ValueError) as exc_info:
+            User.objects.create_superuser(
+                password='TestPass123',
+                mobile='09123456789',
+                is_superuser=False
+                )
+        assert 'Superuser must have is_superuser=True.' in str(exc_info)
+        
+        User.objects.create_superuser(
+                mobile='09123456700',
+                password='TestPass123',
+                )
+        
+
+        
+    def test_authenticate_with_valid_mobile(self):
+        user = User.objects.create_user(mobile='1234567890', password='test123')
+        backend = CustomBackend()
+        
+        authenticated_user = backend.authenticate(request=None, mobile='1234567890')
+        
+        assert authenticated_user == user
+    
+    def test_authenticate_with_invalid_mobile(self):
+        backend = CustomBackend()
+        
+        authenticated_user = backend.authenticate(request=None, mobile='0000000000')
+        
+        assert authenticated_user is None
+    
     def test_register_user(self, api_client):
         url = reverse('register')
         user_mobile = '09123456789'
